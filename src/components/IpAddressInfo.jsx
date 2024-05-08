78c624bf4bbc116247d4f15056ac3e86fad1b8a2
Gustavo Souza <gustavoelia7@gmail.com>
2024-05-08 19:11:23 -0300

    IpAddressInfo and Map components, fixes in the styles

--- src/components/IpAddressInfo.jsx
+++ src/components/IpAddressInfo.jsx
@@ -4,10 +4,22 @@ import "../css/IpAddressInfo.css";
 function IpAddressInfo() {
     return (
         <div className="info">
-            <div className="ip-address"></div>
-            <div className="location"></div>
-            <div className="timezone"></div>
-            <div className="isp"></div>
+            <div className="ip-address">
+                <div className="title">IP Address</div>
+                <div className="data">192.212.174.101</div>
+            </div>
+            <div className="location">
+                <div className="title">Location</div>
+                <div className="data">Brooklyn, NY 10001</div>
+            </div>
+            <div className="timezone">
+                <div className="title">Timezone</div>
+                <div className="data">UTC -05:00</div>
+            </div>
+            <div className="isp">
+                <div className="title">ISP</div>
+                <div className="data">SpaceX Starlink</div>
+            </div>
         </div>
     );
 }
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import "../css/Map.css"
 
 function Map() {
-
+    return (
+        <>
+            <div className="map"></div>
+        </>
+    );
 }
 
 export default Map;
