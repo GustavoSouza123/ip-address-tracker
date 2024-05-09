131a1e159644c1019457f9d0fac46174e2ddf0e9
Gustavo Souza <gustavoelia7@gmail.com>
2024-05-09 09:53:36 -0300

    handling form submission

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import "../css/Search.css"
 
+
 function Search() {
+    function handleSubmit(event) {
+        const ip = document.querySelector('.input').value;
+        console.log('ip:', ip);
+        event.preventDefault();
+    }
+
     return (
-        <form action="">
-            <input type="text" placeholder="Search for any IP address or domain" className="input" />
-            <button className="submit">
+        <form onSubmit={handleSubmit}>
+            <input type="text" name="ip" placeholder="Search for any IP address or domain" className="input" />
+            <button type="submit" className="submit">
                 <img src="./src/assets/icon-arrow.svg" alt="arrow icon" />
             </button>
         </form>
