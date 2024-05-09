1c21a422f0b4b0a9fba736390dc2454b7311c2f0
Gustavo Souza <gustavoelia7@gmail.com>
2024-05-09 08:58:58 -0300

    remove leaflet and react-leaflet packages

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,22 +1,9 @@
 import React from "react";
 import "../css/Map.css";
-import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
 
 function Map() {
     return (
-        <div id="map">
-            <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-                <TileLayer
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-                <Marker position={[51.505, -0.09]}>
-                    <Popup>
-                        A pretty CSS3 popup. <br /> Easily customizable.
-                    </Popup>
-                </Marker>
-            </MapContainer>
-        </div>
+        <div id="map"></div>
     );
 }
 
