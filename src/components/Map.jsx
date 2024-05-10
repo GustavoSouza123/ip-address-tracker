7ec79591b6eddacbc077318ac1d7dfa97e1b8d15
Gustavo Souza <gustavoelia7@gmail.com>
2024-05-10 09:28:05 -0300

    fetching coordinates from geocoding api, showing location of local ip on the map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import "../css/Map.css";
 import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
 
@@ -7,16 +8,35 @@ const mapContainerStyle = {
     width: '100%',
     height: 'calc(100vh - 280px)',
 };
-const center = {
-    lat: 7.2905715, // default latitude
-    lng: 80.6337262, // default longitude
-};
 
-function Map() {
+function Map({ address }) {
+    const [coordinates, setCoordinates] = useState({});
+
     const { isLoaded, loadError } = useLoadScript({
         googleMapsApiKey: 'AIzaSyCiC0gh2jLvKY30FQDM-rFSFzeD91m8flw',
         libraries,
     });
+
+    const center = {
+        lat: coordinates.lat || '', // default latitude
+        lng: coordinates.lng || '', // default longitude
+    };
+
+    useEffect(() => {
+        getCoordinates('São Paulo');
+    }, []);
+    
+    async function getCoordinates(address) {
+        const geoApiKey = 'AIzaSyBZuPdZMbRf0C87FplCamGh8V5VtQNoyLg'; 
+        try {
+            const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${geoApiKey}`);
+            if(!response.ok) throw new Error('there was an error fetching the coordinates of your address');
+            const resCoordinates = await response.json();
+            setCoordinates(resCoordinates.results[0].geometry.location);
+        } catch(err) {
+            console.log('there was an error fetching the coordinates of your address');
+        }
+    }
 
     if(loadError) {
         return <div>Error loading maps</div>
--- src/components/Tracker.jsx
+++ src/components/Tracker.jsx
@@ -11,19 +11,22 @@ function Tracker() {
 
     useEffect(() => {
         getMyIpAddress();
+        // displaying my local ip address info on first load
+        // disabled to not request from api every time and run out of request credits
+        // getIpLocation(myIpAddress);
     }, []);
 
     async function getMyIpAddress() {
         try {
             const response = await fetch('https://api.ipify.org?format=json');
             if(!response.ok) throw new Error('there was an error fetching your local ip address');
-            const myIp = await response.json();
-            setMyIpAddress(myIp.ip);
+            const resIp = await response.json();
+            setMyIpAddress(resIp.ip);
         } catch(err) {
             console.log('there was an error fetching your local ip address')
         }
     }
-
+    
     async function getIpLocation(ipParam) {
         const ip = ipParam || document.querySelector('.input').value;
         const apiKey = 'at_eOp5vIu8GZbLd41oyvdi2FCnHmK3K';
@@ -38,10 +41,6 @@ function Tracker() {
         }
     }
 
-    // displaying my local ip address info on first load
-    // disabled to not request from api every time and run out of request credits
-    // getIpLocation(myIpAddress);
-
     return (
         <div className="container">
             <div className="top">
@@ -55,7 +54,7 @@ function Tracker() {
                     isp: data.isp,
                 }} />
             </div>
-            <Map />
+            <Map address={data != '' && {region: data.location.region}} />
         </div>
     );
 }
